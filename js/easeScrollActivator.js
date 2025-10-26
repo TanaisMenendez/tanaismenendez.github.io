c3e62c6641ef471bcb01180c52ee662a825c702f
TanaisMenendez <tanaismenendez@gmail.com>
2025-10-26 10:51:27 +0100

    Add files via upload

---
+++ js/easeScrollActivator.js
@@ -0,0 +1,16 @@
+$("html").easeScroll();
+
+$("html").easeScroll({
+  frameRate: 60,
+  animationTime: 800,
+  stepSize: 120,
+  pulseAlgorithm: 1,
+  pulseScale: 8,
+  pulseNormalize: 1,
+  accelerationDelta: 20,
+  accelerationMax: 1,
+  keyboardSupport: true,
+  arrowScroll: 50,
+  touchpadSupport: true,
+  fixedBackground: true
+});
