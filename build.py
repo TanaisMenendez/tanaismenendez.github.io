b4285cffc5a3cb53c7dea747ca5ffaa02f0eb5db
TanaisMenendez <tanaismenendez@gmail.com>
2026-02-20 11:27:19 +0100

    update

--- build.py
+++ build.py
@@ -15,6 +15,7 @@ STUB_TEMPLATE = """<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>{title_stub}</title>
     <meta name="description" content="{desc_clean}">
 
